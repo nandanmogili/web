bc2b39dc70c007bc72c84ce764850eb1ac67439e
Nandan Sai Mogili <nandan.mogili@gmail.com>
2024-07-14 02:46:47 +0530

    first commit

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -5,10 +5,10 @@ import './SocialIcons.css';
 const SocialIcons = () => {
   return (
     <div className="social-icons">
-      <a href="https://www.facebook.com/nandanmogili" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.facebook.com/profile.php?id=100011179215456" target="_blank" rel="noopener noreferrer">
         <FaFacebook />
       </a>
-      <a href="https://www.linkedin.com/in/nandan-mogili/" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.linkedin.com/in/nandan-mogili-67a12a1b2" target="_blank" rel="noopener noreferrer">
         <FaLinkedin />
       </a>
       <a href="https://github.com/nandanmogili" target="_blank" rel="noopener noreferrer">
